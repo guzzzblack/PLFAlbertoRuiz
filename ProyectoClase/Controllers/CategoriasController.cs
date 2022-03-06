15cd9fe594a3af20517bd0d669127f7a2daad747
jesus alberto ruiz de los angeles <ruizjesus666@gmail.com>
2022-03-06 12:23:54 -0600

    Cambios de prueba en el proyecto

---
+++ ProyectoClase/Controllers/CategoriasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoClase.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public IActionResult Nuevacategoria()
+        {
+            return View();
+        }
+        public IActionResult Vercategoria()
+        {
+            return View();
+        }
+        public IActionResult Permitircategoria()
+        {
+            return View();
+        }
+    }
+}
--- ProyectoClase/Controllers/TareasController.cs
+++ ProyectoClase/Controllers/TareasController.cs
@@ -9,50 +9,17 @@ namespace ProyectoClase.Controllers
 {
     public class TareasController : Controller
     {
-        // MANDAR A LLAMAR A LA FUNCION CONTEXT 
-        private readonly ProyectoDbContext _context;
-        private readonly IUsuarioService _usuarioservice;
-        public TareasController(ProyectoDbContext context, IUsuarioService usuarioservice)
-        { 
-            _usuarioservice = usuarioservice;
-            _context = context; 
-        }
-        public IActionResult Index()
+        public IActionResult ListaTareas()
         {
             return View();
         }
-        public IActionResult Registrarse()
+        public IActionResult Tareas()
         {
             return View();
         }
-        [HttpPost]
-        public IActionResult Insert(TareasViewModel model)
+        public IActionResult Creartarea()
         {
-            if (!ModelState.IsValid)
-            {
-                return View("RegistroTarea", model);
-            }
-            var result = _context.Tareas.Where(x => x.descripcion == model.descripcion);
-            if (result.Count() == 0)
-            {
-                var tarea = new Tareas
-                {
-                    estadotarea = model.estadotarea,
-                    fechainicio = model.fechainicio,
-                    prioridad = model.prioridad,
-                    descripcion = model.descripcion,
-                    idusuariocreador = model.idusuariocreador,
-                    idusuarioasignado = model.idusuarioasignado,
-                    archivado = true
-                };
-                //_usuarioservice.Create(tarea);
-                return RedirectToAction("Index", "Principal");
-            }
-            else
-            {
-                return View("Registrarse", new { message = "Usuario Existente" });
-            }
+            return View();
         }
-
     }
 }
